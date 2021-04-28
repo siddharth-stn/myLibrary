28346bd66e7eacd38b8e938bf579842d57d20d80
siddharth-stn <66431369+siddharth-stn@users.noreply.github.com>
2021-04-28 17:13:25 +0530

    Database code removed

--- library.js
+++ library.js
@@ -1,22 +1,11 @@
 let myLibrary = [];
 
-var database = firebase.database();
-
-function writeUserData(counter, title, author, totPages, read) {
-    database.ref('Book/' + counter).set({
-      title: title,
-      author: author,
-      totPages : totPages,
-      read: read
-    });
-  }
-
-function Book (title, author, totPages, read) {
-    this.title = title;
-    this.author = author;
-    this.totPages = totPages;
-    this.read = read;
-};
+function Book(title, author, totPages, read) {
+  this.title = title;
+  this.author = author;
+  this.totPages = totPages;
+  this.read = read;
+}
 
 let newBookBtn = document.getElementById("newBookBtn");
 let formDiv = document.querySelector(".form-div");
@@ -29,81 +18,92 @@ let containerDiv = document.querySelector(".container-div");
 let counter = 1;
 
 newBookBtn.addEventListener("click", () => {
-    formDiv.classList.toggle("hidden");
-    main.classList.toggle("margin-top-for-fixed-header");
+  formDiv.classList.toggle("hidden");
+  main.classList.toggle("margin-top-for-fixed-header");
 });
 
 submitBtn.addEventListener("click", () => {
-
-    myLibrary[counter] = new Book(title.value, author.value, pages.value, "Not Read");
-    
-    writeUserData(counter, title.value, author.value, pages.value,"Not Read");
-
-   let cardDiv = document.createElement("div");
-   let titlePara = document.createElement("p");
-   let authorPara = document.createElement("p");
-   let pagesPara = document.createElement("p");
-   let readPara = document.createElement("p");
-   let toggleReadBtn = document.createElement("button");
-   let removebtn = document.createElement("button");
-   let counterPara = document.createElement("p");
+  myLibrary[counter] = new Book(
+    title.value,
+    author.value,
+    pages.value,
+    "Not Read"
+  );
 
 
-   cardDiv.classList.add("card");
-   titlePara.classList.add("title");
-   authorPara.classList.add("author");
-   pagesPara.classList.add("pages");
-   readPara.classList.add("read");
-   toggleReadBtn.classList.add("read-button");
-   removebtn.classList.add("remove-btn");
-   counterPara.classList.add("hidden-elem");
+  let cardDiv = document.createElement("div");
+  let titlePara = document.createElement("p");
+  let authorPara = document.createElement("p");
+  let pagesPara = document.createElement("p");
+  let readPara = document.createElement("p");
+  let toggleReadBtn = document.createElement("button");
+  let removebtn = document.createElement("button");
+  let counterPara = document.createElement("p");
 
-   titlePara.appendChild(document.createTextNode(title.value));
-   authorPara.appendChild(document.createTextNode("Authored by: " + author.value));
-   pagesPara.appendChild(document.createTextNode("No. of Pages: " + pages.value));
-   let readNode = document.createTextNode("Not Read");
-   readPara.appendChild(readNode);
-   toggleReadBtn.innerHTML = "Toggle Read";
-   removebtn.innerHTML = "Remove";
-   counterPara.appendChild(document.createTextNode(counter));
+  cardDiv.classList.add("card");
+  titlePara.classList.add("title");
+  authorPara.classList.add("author");
+  pagesPara.classList.add("pages");
+  readPara.classList.add("read");
+  toggleReadBtn.classList.add("read-button");
+  removebtn.classList.add("remove-btn");
+  counterPara.classList.add("hidden-elem");
 
-   cardDiv.appendChild(counterPara);
-   cardDiv.appendChild(titlePara);
-   cardDiv.appendChild(authorPara);
-   cardDiv.appendChild(pagesPara);
-   cardDiv.appendChild(readPara);
-   cardDiv.appendChild(toggleReadBtn);
-   cardDiv.appendChild(removebtn);
-   containerDiv.appendChild(cardDiv);
+  titlePara.appendChild(document.createTextNode(title.value));
+  authorPara.appendChild(
+    document.createTextNode("Authored by: " + author.value)
+  );
+  pagesPara.appendChild(
+    document.createTextNode("No. of Pages: " + pages.value)
+  );
+  let readNode = document.createTextNode("Not Read");
+  readPara.appendChild(readNode);
+  toggleReadBtn.innerHTML = "Toggle Read";
+  removebtn.innerHTML = "Remove";
+  counterPara.appendChild(document.createTextNode(counter));
 
-   formDiv.classList.add("hidden");
-   main.classList.add("margin-top-for-fixed-header");
-   counter += 1;
+  cardDiv.appendChild(counterPara);
+  cardDiv.appendChild(titlePara);
+  cardDiv.appendChild(authorPara);
+  cardDiv.appendChild(pagesPara);
+  cardDiv.appendChild(readPara);
+  cardDiv.appendChild(toggleReadBtn);
+  cardDiv.appendChild(removebtn);
+  containerDiv.appendChild(cardDiv);
+
+  formDiv.classList.add("hidden");
+  main.classList.add("margin-top-for-fixed-header");
+  counter += 1;
 });
 
 document.addEventListener("click", (e) => {
-   let parentElement = e.target.parentElement;
-   let readElem = parentElement.childNodes;
-   if (parentElement.classList == "card" && e.target.classList == "read-button") {
+  let parentElement = e.target.parentElement;
+  let readElem = parentElement.childNodes;
+  if (
+    parentElement.classList == "card" &&
+    e.target.classList == "read-button"
+  ) {
     let index = readElem[0].innerHTML;
     index = Number(index);
     //document.getElementById("newPara").appendChild(document.createTextNode(index));
-        if (readElem[4].innerHTML == "I have read this") {
-            readElem[4].innerHTML = "Not Read";
-            myLibrary[index].read = "Not Read";
-        } else if (readElem[4].innerHTML == "Not Read") {
-            readElem[4].innerHTML = "I have read this";
-            myLibrary[index].read = "I have read this";
-        }
-    } else if (parentElement.classList == "card" && e.target.classList == "remove-btn") {
-        let superParent = parentElement.parentNode;
-        superParent.removeChild(parentElement);
+    if (readElem[4].innerHTML == "I have read this") {
+      readElem[4].innerHTML = "Not Read";
+      myLibrary[index].read = "Not Read";
+    } else if (readElem[4].innerHTML == "Not Read") {
+      readElem[4].innerHTML = "I have read this";
+      myLibrary[index].read = "I have read this";
     }
+  } else if (
+    parentElement.classList == "card" &&
+    e.target.classList == "remove-btn"
+  ) {
+    let superParent = parentElement.parentNode;
+    superParent.removeChild(parentElement);
+  }
 });
 
-function showBooks () {
-    myLibrary.forEach(element => {
-        console.log(element);
-    });
-};
-
+function showBooks() {
+  myLibrary.forEach((element) => {
+    console.log(element);
+  });
+}
