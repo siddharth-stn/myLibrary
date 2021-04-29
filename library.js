cb48c732d10c6ed6c081850e51a828bfc06752e4
siddharth-stn <66431369+siddharth-stn@users.noreply.github.com>
2021-04-29 19:25:28 +0530

    project complete

--- library.js
+++ library.js
@@ -1,4 +1,3 @@
-
 // *** Initialized the firebase database into the variable named db ----->
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
@@ -6,7 +5,8 @@ const db = firebase.firestore();
 
 // *** Function to add books from the databse to array ------>
 async function addBooksFromDbToArray() {
-  await db.collection("Books")
+  await db
+    .collection("Books")
     .get()
     .then((snapShot) => {
       snapShot.forEach((doc) => {
@@ -67,7 +67,7 @@ addBooksFromDbToArray().then(() => {
   myLibrary.forEach((book) => {
     createBooks(book.title, book.author, book.totPages, book.read);
   });
-}); 
+});
 //---------------XXXXXXXXXXXX---------------XXXXXXXXXXXX---------------XXXXXXXXXXXX
 
 // *** Constructor function (class) to create new books ------>
@@ -100,41 +100,62 @@ newBookBtn.addEventListener("click", () => {
 
 // *** Add the data of new book from form to database and show on the page -------->
 submitBtn.addEventListener("click", () => {
-  db.collection("Books").add({
-    title: title.value,
-    author: author.value,
-    totPages: pages.value,
-    read: "Not Read",
+  let snapShot = db.collection("Books").where("title", "==", title.value).get();
+  snapShot.then((snap) => {
+    if (snap.empty) {
+      db.collection("Books").add({
+        title: title.value,
+        author: author.value,
+        totPages: pages.value,
+        read: "Not Read",
+      });
+      createBooks(title.value, author.value, pages.value, "Not Read");
+    } else
+      alert(
+        "Book having same title already in library, choose another book to add."
+      );
   });
-
-  createBooks(title.value, author.value, pages.value, "Not Read");
 
   formDiv.classList.add("hidden");
   main.classList.add("margin-top-for-fixed-header");
 });
 //---------------XXXXXXXXXXXX---------------XXXXXXXXXXXX---------------XXXXXXXXXXXX
 
+// *** toggle read button code to toggle the book read or not ----->
 document.addEventListener("click", (e) => {
-  let parentElement = e.target.parentElement;
-  let readElem = parentElement.childNodes;
-  if (
-    parentElement.classList == "card" &&
-    e.target.classList == "read-button"
-  ) {
-    let index = readElem[0].innerHTML;
-    index = Number(index);
-    if (readElem[4].innerHTML == "I have read this") {
-      readElem[4].innerHTML = "Not Read";
-      myLibrary[index].read = "Not Read";
-    } else if (readElem[4].innerHTML == "Not Read") {
-      readElem[4].innerHTML = "I have read this";
-      myLibrary[index].read = "I have read this";
+  if (e.target.classList == "read-button") {
+    let parentElement = e.target.parentElement;
+    let cardTitle = parentElement.children[0].innerText;
+    let readKey = parentElement.children[3].innerText;
+    if (readKey === "Not Read") {
+      parentElement.children[3].innerText = "I have read this";
+    } else {
+      parentElement.children[3].innerText = "Not Read";
     }
-  } else if (
-    parentElement.classList == "card" &&
-    e.target.classList == "remove-btn"
-  ) {
-    let superParent = parentElement.parentNode;
-    superParent.removeChild(parentElement);
+    let snapShot = db.collection("Books").where("title", "==", cardTitle).get();
+    snapShot.then((snap) => {
+      snap.forEach((doc) => {
+        db.collection("Books").doc(doc.id).update({
+          read: parentElement.children[3].innerText,
+        });
+      });
+    });
   }
 });
+//---------------XXXXXXXXXXXX---------------XXXXXXXXXXXX---------------XXXXXXXXXXXX
+
+// *** remove button to remove book from the database, from the array and from the page ----->
+document.addEventListener("click", (e) => {
+  if (e.target.classList == "remove-btn") {
+    let parentElement = e.target.parentElement;
+    let cardTitle = parentElement.children[0].innerText;
+    let snapShot = db.collection("Books").where("title", "==", cardTitle).get();
+    snapShot.then((snap) => {
+      snap.forEach((doc) => {
+        db.collection("Books").doc(doc.id).delete();
+      });
+    });
+    parentElement.remove();
+  }
+});
+//---------------XXXXXXXXXXXX---------------XXXXXXXXXXXX---------------XXXXXXXXXXXX
